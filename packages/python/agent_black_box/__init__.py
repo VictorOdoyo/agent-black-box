cbe4a94513c134d5d35f9a0cec6965020742dcce
Victor Odoyo <victorodoyo555@gmail.com>
2026-09-07 10:20:11 +0300

    feat: export advanced sdk helpers

--- packages/python/agent_black_box/__init__.py
+++ packages/python/agent_black_box/__init__.py
@@ -1,24 +1,62 @@
+from .artifacts import ArtifactRecord, artifact_manifest, verify_artifact_files
+from .bundles import create_trace_bundle
+from .causality import CausalEdge, ancestors, build_causal_edges, explain_event
 from .compare import TraceComparison, compare_traces
+from .filters import filter_events
+from .integrity import IntegrityFinding, chain_hash, trace_manifest, verify_event_fingerprints, verify_manifest
+from .metrics import TraceMetrics, calculate_metrics
 from .models import EventKind, TraceEnvelope, TraceEvent
+from .permissions import CapabilityRecord, capability_ledger, unauthorized_tool_calls
 from .policy import PolicyFinding, evaluate_trace
+from .policy_packs import PolicyPack, evaluate_policy_pack, load_policy_pack
 from .recorder import AgentRecorder
 from .redaction import RedactionPolicy, redact_value
 from .replay import ReplaySession, fork_trace, replay_plan
 from .storage import TraceFileStore
+from .stream import read_jsonl, write_jsonl
+from .timeline import actor_timeline, events_between, run_duration_seconds, sorted_events
 
 __all__ = [
     "AgentRecorder",
+    "ArtifactRecord",
+    "CapabilityRecord",
+    "CausalEdge",
     "EventKind",
+    "IntegrityFinding",
     "PolicyFinding",
+    "PolicyPack",
     "RedactionPolicy",
     "ReplaySession",
     "TraceComparison",
     "TraceEnvelope",
     "TraceEvent",
     "TraceFileStore",
+    "TraceMetrics",
+    "actor_timeline",
+    "ancestors",
+    "artifact_manifest",
+    "build_causal_edges",
+    "calculate_metrics",
+    "capability_ledger",
+    "chain_hash",
     "compare_traces",
+    "create_trace_bundle",
+    "evaluate_policy_pack",
     "evaluate_trace",
+    "events_between",
+    "explain_event",
+    "filter_events",
     "fork_trace",
+    "load_policy_pack",
+    "read_jsonl",
     "redact_value",
     "replay_plan",
+    "run_duration_seconds",
+    "sorted_events",
+    "trace_manifest",
+    "unauthorized_tool_calls",
+    "verify_artifact_files",
+    "verify_event_fingerprints",
+    "verify_manifest",
+    "write_jsonl",
 ]
